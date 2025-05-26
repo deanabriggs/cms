d070431330d0cda944dfdd341a1c16f1d3611b4c
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-05-26 12:33:50 -0700

    Navigation and main form views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
+  selectedFeature: string = "documents";  // variable to hold the selected feature, default to "documents"
+
+  switchView(feature: string){
+    this.selectedFeature = feature;  // sets the local variable "selectedFeature" to the parameter when the function is called
+  }
 }

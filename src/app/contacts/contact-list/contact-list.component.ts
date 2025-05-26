ce02c86ce8e43eeb71e4157c711c4d5e4e8f7662
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-05-25 22:43:25 -0700

    Modulated the Contact components

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -8,8 +8,15 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+  @Output() selectedContactEvent = new EventEmitter<Contact>();  // holder for the data to be "Output" to the parent and pushes on event
+  
+  
   contacts: Contact[] = [
     new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "images/jacksonk.jpg"),
     new Contact(2, "Rex Barzee", "bbarzeer@byui.edu", "208-496-3768", "images/barzeer.jpg")
   ];
+
+  onSelected(contact: Contact){   // method used to accept data from the DOM and push to EventEmitter
+    this.selectedContactEvent.emit(contact); 
+  }
 }

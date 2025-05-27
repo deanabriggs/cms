977ef7b1349ccd30e26ee613cf31fd0013971b4f
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-05-27 00:13:36 -0700

    Added documents functionality and content and directive for menu dropdown

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(1, "Application", "Job Application Template", "www.applications.org"),
+    new Document(2, "W4", "Payroll Tax Form", "www.irs.gov/w4"),
+    new Document(3, "Benefits", "Benefits offered", "www.company.com/benefits"),
+    new Document(4, "Handbook", "Employee Handbook", "www.company.com/handbook"),
+    new Document(5, "ACH", "Direct Deposit Form", "www.company.com/ACH")
+  ]
+
+    onSelectedDocument(document:Document){
+      this.selectedDocumentEvent.emit(document);
+    }
 }

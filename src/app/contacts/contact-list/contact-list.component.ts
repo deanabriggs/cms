77dfd25aac01202a3d9573396fca06557e296e9f
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-05-29 20:30:23 -0700

    Use a mockcontacts and created contact.service

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,21 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Injectable, OnInit, Output } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
+@Injectable()
 @Component({
   selector: 'cms-contact-list',
   standalone: false,
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-export class ContactListComponent {
-  @Output() selectedContactEvent = new EventEmitter<Contact>();  // holder for the data to be "Output" to the parent and pushes on event
-  
-  contacts: Contact[] = [
-    new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "images/jacksonk.jpg"),
-    new Contact(2, "Rex Barzee", "bbarzeer@byui.edu", "208-496-3768", "images/barzeer.jpg")
-  ];
 
-  onSelected(contact: Contact){   // method used to accept data from the DOM and push to EventEmitter
-    this.selectedContactEvent.emit(contact); 
+export class ContactListComponent implements OnInit{
+  
+  contacts: Contact[] = [];
+  constructor(private contactService: ContactService){}
+
+  ngOnInit(): void {
+    this.contacts = this.contactService.getContacts();
   }
 }

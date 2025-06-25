1d7d4553b2fe6389b2bb465a26d2909651394bfb
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-06-24 22:11:48 -0700

    Add, edit, delete documents and contacts

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Contact } from '../contact.model';
+import { NgForm } from '@angular/forms';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
@@ -7,13 +10,63 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private contactService: ContactService,
+    private route: ActivatedRoute,
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          if(!params.id) {
+            this.editMode = false;
+            return;
+          }
+          this.originalContact = this.contactService.getContact(params.id);
+
+          if(!this.originalContact) {
+            return;
+          }
+          this.editMode = true;
+          this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+          if(this.contact.group) {
+            this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+          }
+        }
+      )
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    let contact = new Contact(
+      "0",
+      this.contact.name,
+      this.contact.email,
+      this.contact.phone,
+      this.contact.imageUrl,
+      this.contact.group
+    );
+
+    if(this.editMode == true) {
+      this.contactService.updateContacts(this.originalContact, contact);
+    } else {
+      this.contactService.addContact(contact);
+    };
+    this.editMode = false;
+    this.router.navigate(['/contacts']);
+  }
+
   onCancel() {
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['/contacts']);
   }
 }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -8,30 +8,41 @@ import { Subscription } from 'rxjs';
   selector: 'cms-contact-list',
   standalone: false,
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
 
 export class ContactListComponent implements OnInit, OnDestroy{
   private subscription: Subscription;
   contacts: Contact[] = [];
+  individualContacts: Contact[] = [];
+  groupedContacts: Contact[] = [];
 
   constructor(private contactService: ContactService){}
 
   ngOnInit(): void {
     this.contacts = this.contactService.getContacts();
-    
-    // this.contactService.contactChangedEvent
-    //   .subscribe((cts: Contact[]) => {
-    //     this.contacts = cts;
-    //   })
+    this.separateContacts(this.contacts)
     
     this.subscription = this.contactService.contactListChangedEvent
       .subscribe((cts: Contact[]) => {
         this.contacts = cts;
+        this.separateContacts(cts);
       })
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  private separateContacts(contacts: Contact[]) {
+    this.groupedContacts = contacts.filter(c => Array.isArray(c.group) && c.group.length > 0);
+
+    const groupMemberIds = new Set(
+      this.groupedContacts.flatMap(group => group.group.map(member => String(member.id)))
+    );
+
+    this.individualContacts = contacts.filter(
+      c => !Array.isArray(c.group) && !groupMemberIds.has(String(c.id))
+    )
   }
 }
